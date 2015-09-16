ea8a9d61f364c8fec09d6d56bd70f0ed4db63b48
sauchye <sauchyer@gmail.com>
2015-09-16 16:51:04 +0800

    Initial Commit

---
+++ SYDynamicTextDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SYDynamicTextDemo
+//
+//  Created by Saucheong Ye on 9/16/15.
+//  Copyright (c) 2015 sauchye.com. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ SYDynamicTextDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  SYDynamicTextDemo
+//
+//  Created by Saucheong Ye on 9/16/15.
+//  Copyright (c) 2015 sauchye.com. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
