296f2c45bd83b5e209369ed0163eb2bf02c6f5a2
sauchye <sauchyer@gmail.com>
2015-09-16 16:51:53 +0800

    add

--- SYDynamicTextDemo/ViewController.m
+++ SYDynamicTextDemo/ViewController.m
@@ -6,7 +6,16 @@
 //  Copyright (c) 2015 sauchye.com. All rights reserved.
 //
 
+#define SCREEN_WIDTH ([UIScreen mainScreen].bounds.size.width)
+#define SCREEN_HEIGHT ([UIScreen mainScreen].bounds.size.height)
+
 #import "ViewController.h"
+
+@interface ViewController ()
+
+@property (nonatomic, strong) UILabel *textLbl;
+
+@end
 
 @interface ViewController ()
 
@@ -16,12 +25,41 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    
+    
+    _textLbl = [[UILabel alloc] init];
+    
+    _textLbl.text = @"sdakjgauisdguasfduysaruydryasrd7yqrew67e4721834612431256473qwgeuysadkjguasfduafsduyafysyatsdasydfasytdasdky..\nqwigegqweigfqwufeuyqfwuefuqywe81263812835uafdhasfdyaysdfyasfdy\naskdhaiusgdigausif";
+    
+    
+    _textLbl.numberOfLines = 0;
+    
+    _textLbl.lineBreakMode = NSLineBreakByCharWrapping;
+    
+    _textLbl.textColor = [UIColor orangeColor];
+    _textLbl.font = [UIFont systemFontOfSize:15.0];
+    
+    
+    _textLbl.backgroundColor = [UIColor grayColor];
+    
+    
+    CGFloat textHeight = [self heightForText:_textLbl.text fontSize:15.0];
+    
+    _textLbl.frame = CGRectMake(10, 20, SCREEN_WIDTH-20, textHeight);
+    
+    
+    [self.view addSubview:_textLbl];
+    
 }
 
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+
+- (CGFloat)heightForText:(NSString *)text fontSize:(CGFloat)fontSize
+{
+    
+    NSDictionary *attrbute = @{NSFontAttributeName:[UIFont systemFontOfSize:fontSize]};
+    
+    return ceilf([text boundingRectWithSize:CGSizeMake(SCREEN_WIDTH-20, CGFLOAT_MAX) options:NSStringDrawingUsesLineFragmentOrigin attributes:attrbute context:nil].size.height);
 }
+
 
 @end
